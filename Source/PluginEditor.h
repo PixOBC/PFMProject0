9a43470b4df08ea1b67b114f4aabcd6a66f5bfc2
James Pickering <james@oneblankchannel.com>
2021-02-20 15:31:35 +0000

    Initial commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PMFProject0AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    PMFProject0AudioProcessorEditor (PMFProject0AudioProcessor&);
+    ~PMFProject0AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PMFProject0AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMFProject0AudioProcessorEditor)
+};
